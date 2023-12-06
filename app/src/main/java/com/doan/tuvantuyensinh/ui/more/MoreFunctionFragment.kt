919f79f4136068e37c84d2185e1af551ac975e04
lenhathuy <huyhuy2899@gmail.com>
2023-12-06 08:57:16 +0700

    [ADD] bổ dung đường vào từ màn hình danh sách

--- app/src/main/java/com/doan/tuvantuyensinh/ui/more/MoreFunctionFragment.kt
+++ app/src/main/java/com/doan/tuvantuyensinh/ui/more/MoreFunctionFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.doan.tuvantuyensinh.R
 import com.doan.tuvantuyensinh.ui.more.MoreFunctionViewModel
 import com.doan.tuvantuyensinh.databinding.FragmentMoreFunctionBinding
 import com.doan.tuvantuyensinh.ui.home.function.FunctionData
@@ -38,9 +40,11 @@ class MoreFunctionFragment : Fragment() {
         with(binding) {
             userFunctionRecyclerView.apply {
                 val listFunctionData = listOf(
-                    FunctionData("Thông tin cá nhân", "ic_user"),
-                    FunctionData("Đổi mật khẩu", "ic_password"),
-                    FunctionData("Đăng xuất", "ic_logout")
+                    FunctionData("Xem thông tin trường", "ic_truong_hoc",0),
+                    FunctionData("Thông tin nghề nghiệp liên quan", "ic_job", 1),
+                    FunctionData("Thông tin học phí", "ic_hoc_phi", 2),
+                    FunctionData("Thông tin học bổng", "ic_hoc_bong", 3),
+                    FunctionData("Thông tin chỉ tiêu", "ic_chi_tieu", 4),
                 )
                 adapter = MoreFunctionAdapter(listFunctionData).apply {
                     onClickFunction = {
@@ -52,24 +56,36 @@ class MoreFunctionFragment : Fragment() {
             }
 
             val listFunctionDataAdmin = listOf(
-                FunctionData("Quản lý ngành", "ic_major"),
-                FunctionData("Quản lý trường", "ic_school"),
-                FunctionData("Quản lý người dùng", "ic_user"),
-                FunctionData("Quản lý tin tức", "ic_news"),
-                FunctionData("Quản lý hướng dẫn", "ic_guide"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
-                FunctionData("Quản lý đơn xét tuyển", "ic_apply"),
+                FunctionData("Quản lý trường", "ic_school", 10),
+                FunctionData("Quản lý nghề", "ic_job", 11),
+                FunctionData("Quản lý học phí", "ic_fee", 12),
+                FunctionData("Quản lý học bổng", "ic_scholarship", 13),
+                FunctionData("Quản lý chỉ tiêu", "ic_target", 14),
+                FunctionData("Quản lý ngành", "ic_major", 15),
             )
             adminFunctionRecyclerView.apply {
                 adapter = MoreFunctionAdapter(listFunctionDataAdmin).apply {
                     onClickFunction = {
-
+                        when(it.functionId) {
+                            10 -> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_navigation_school_info)
+                            }
+                            11 -> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_navigation_job)
+                            }
+                            12-> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_navigation_tuition)
+                            }
+                            13 -> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_navigation_scholarship)
+                            }
+                            14 -> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_navigation_target)
+                            }
+                            else -> {
+                                findNavController().navigate(R.id.action_more_function_fragment_to_undevelopFragment)
+                            }
+                        }
                     }
                 }
                 layoutManager = LinearLayoutManager(requireContext())
